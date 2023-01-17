3133f81c541c90fb4c520d23f48e5d15bc75f578
EventH0rizonz <adhansen13@gmail.com>
2023-01-17 07:31:54 -0700

    fix 5

--- scripts/date.js
+++ scripts/date.js
@@ -2,6 +2,10 @@ function displayLastModified() {
     const lastModified = document.lastModified;
     const lastModifiedElement = document.getElementById("last-modified");
     lastModifiedElement.innerHTML = `Last modified: ${lastModified}`;
+    let date =  new Date().getFullYear();
+    const dateYear = document.getElementById("year-Name")
+    dateYear.innerHTML = `© ${date} | Alexander Hansen | Arizona`;
+
   }
   
 
