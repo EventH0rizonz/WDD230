6c381760a0cbfe7bdd298a97063d0a271cabf5c4
EventH0rizonz <adhansen13@gmail.com>
2023-01-28 22:38:10 -0700

    week 4 homepage upddate

---
+++ soon_to_be_Chamber/scripts/date.js
@@ -0,0 +1,14 @@
+function displayLastModified() {
+    const lastModified = document.lastModified;
+    const lastModifiedElement = document.getElementById("last-modified");
+    lastModifiedElement.innerHTML = `Last modified: ${lastModified}`;
+    let date =  new Date().getFullYear();
+    const dateYear = document.getElementById("year-Name")
+    dateYear.innerHTML = `© ${date} Tucson Chamber | Alexander Hansen | WDD 230 Project`;
+    var today = new Date();
+    var date1 = today.toLocaleDateString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})
+    document.getElementById("date").innerHTML = date1;
+  }
+  
+
+window.onload = displayLastModified;
