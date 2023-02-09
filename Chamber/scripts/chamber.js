6b093cb6e1d8299b2bc0705c6f60e9fc17a719b6
EventH0rizonz <adhansen13@gmail.com>
2023-02-08 20:04:03 -0700

    Week 3 update

--- Chamber/scripts/chamber.js
+++ Chamber/scripts/chamber.js
@@ -7,8 +7,11 @@ function displayLastModified() {
     dateYear.innerHTML = `© ${date} Tucson Chamber | Alexander Hansen | WDD 230 Project`;
     var today = new Date();
     var date1 = today.toLocaleDateString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})
-    document.getElementById("date").innerHTML = date1;
-  }
-  
+    document.getElementById("date").innerHTML = date1 + " Hello world";
 
+  }
 window.onload = displayLastModified;
+
+let banner = document.getElementById("banner");
+banner.innerHTML = "banner";
+
